b56690101ffc9e5de1197d636c2ce36f08c59475
Aaron Lee <aaronlee97667@gmail.com>
2023-03-09 15:40:05 -0600

    Added Visual Elements to Main Page

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,3 +1,6 @@
+import * as LottiePlayer from "@lottiefiles/lottie-player";
+import "./features/features.css"
+
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
@@ -7,6 +10,14 @@ function MainPage() {
           The premiere solution for automobile dealership
           management!
         </p>
+      </div>
+      <div className="player">
+        <lottie-player
+          autoplay
+          loop
+          src="https://assets5.lottiefiles.com/packages/lf20_CZyPeqwmRW.json"
+          style={{}}
+        ></lottie-player>
       </div>
     </div>
   );
