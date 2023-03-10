78c8f4050ebee182043361c9a3854562df118410
Aaron Lee <aaronlee97667@gmail.com>
2023-03-10 10:27:33 -0600

    Refactored main page

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,25 +1,29 @@
-import * as LottiePlayer from "@lottiefiles/lottie-player";
-import "./features/features.css"
+import { Player } from "@lottiefiles/react-lottie-player";
+import car from "./lottieimg/car.json"
+import { Container } from 'react-bootstrap'
+
 
 function MainPage() {
   return (
-    <div className="px-4 py-5 my-5 text-center">
-      <h1 className="display-5 fw-bold">CarCar</h1>
-      <div className="col-lg-6 mx-auto">
-        <p className="lead mb-4">
-          The premiere solution for automobile dealership
-          management!
-        </p>
+    <Container>
+      <div className="px-4 py-5 my-5 text-center">
+        <h1 className="display-5 fw-bold">CarCar</h1>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-4">
+            The premiere solution for automobile dealership
+            management!
+          </p>
+        </div>
+        <div>
+          <Player
+            autoplay
+            loop
+            src={car}
+            style={{ height:"400px", width:"700px"}}
+          ></Player>
+        </div>
       </div>
-      <div className="player">
-        <lottie-player
-          autoplay
-          loop
-          src="https://assets5.lottiefiles.com/packages/lf20_CZyPeqwmRW.json"
-          style={{}}
-        ></lottie-player>
-      </div>
-    </div>
+    </Container>
   );
 }
 
